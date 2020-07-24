f3318221f12d749dfad337d476610526aa08ef5b
xhonker <wu_xhonker@hotmail.com>
2020-07-24 17:38:02 +0800

    chore:sftp log

--- src/commo/sftp-client.ts
+++ src/commo/sftp-client.ts
@@ -33,8 +33,7 @@ export class SftpClient extends BaseClient {
       })
 
     } catch (_) {
-      console.log("Quicker: SftpClient -> _", _)
-
+      log.error(`sftp upload err${_}`)
     }
   }
   connect(opts: SFtpOptions): Promise<boolean> {
