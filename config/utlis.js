08d68524abbb30f019d31dddbbdb8955193e8c8c
xhonker <wu_xhonker@hotmail.com>
2020-09-03 18:33:11 +0800

    refactor:utils

--- config/utlis.js
+++ config/utlis.js
@@ -1,11 +1,9 @@
-export function deepMerge(obj1, obj2) {
-  var key;
-  for (key in obj2) {
-    // 如果target(也就是obj1[key])存在，且是对象的话再去调用deepMerge，否则就是obj1[key]里面没这个对象，需要与obj2[key]合并
-    obj1[key] =
-      obj1[key] && obj1[key].toString() === '[object Object]'
-        ? deepMerge(obj1[key], obj2[key])
-        : (obj1[key] = obj2[key]);
-  }
+const is = {
+  obj: (a) => Object.prototype.toString.call(a) === '[object Object]',
+};
+export const deepMerge = (obj1, obj2) => {
+  Object.keys(obj2).forEach((k) => {
+    obj1[k] = (obj1[k] && is.obj(obj1[k]) && deepMerge(obj1[k], obj2[k])) || obj2[k];
+  });
   return obj1;
-}
+};
